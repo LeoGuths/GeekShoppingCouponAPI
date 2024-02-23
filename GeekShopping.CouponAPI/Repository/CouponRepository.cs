fb7d4ed5b2eb3b546462bddfba08abd531e65a54
Leonardo Güths <leonardoguths@gmail.com>
2024-02-23 18:14:23 -0300

    first commit

---
+++ GeekShopping.CouponAPI/Repository/CouponRepository.cs
@@ -0,0 +1,19 @@
+﻿using GeekShopping.CouponAPI.Data.ValueObjects;
+using GeekShopping.CouponAPI.Model.Context;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CouponAPI.Repository; 
+
+public class CouponRepository : ICouponRepository {
+    private readonly MySqlContext _context;
+
+    public CouponRepository(MySqlContext context) {
+        _context = context;
+    }
+    
+    public async Task<CouponVO> GetCouponByCouponCode(string couponCode) {
+        var coupon = await _context.Coupons.FirstOrDefaultAsync(c => c.CouponCode == couponCode);
+        return coupon.Adapt<CouponVO>();
+    }
+}
